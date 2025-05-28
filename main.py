769b208b3518eee25e6c75c4cdeaefded1121963
otabek-a <otabekabdurasulov245@gmail.com>
2025-05-28 20:55:10 +0500

    qwerty

---
+++ main.py
@@ -0,0 +1,46 @@
+# Function to get capitalized female names from a CSV string
+def read_data(file_path):
+  """
+read csv file from file_path and return list 
+
+  """
+  return data
+data=read_data("people_info.csv")
+def get_female_names_capitalized(data: str) -> list:
+    pass
+
+# Function to get a dictionary of male names (capitalized) and their phone numbers from a CSV string
+def get_male_phone_info(data: str) -> dict:
+    pass
+
+# Function to get occupations starting with a vowel from a CSV string
+def get_occupations_starting_with_vowels(data: str) -> str:
+    pass
+
+# Function to return the number of entries in the CSV
+def get_total_entries(data: str) -> int:
+    pass
+
+# Function to return a list of all countries mentioned
+def get_all_countries(data: str) -> list:
+    pass
+
+# Function to return a set of unique occupations
+def get_unique_occupations(data: str) -> set:
+    pass
+
+# Function to return the first name of the first person in the CSV
+def get_first_entry_first_name(data: str) -> str:
+    pass
+
+# Function to return the average length of first names
+def get_average_first_name_length(data: str) -> float:
+    pass
+
+# Function to return True if 'peter' is in any name (case insensitive)
+def contains_peter(data: str) -> bool:
+    pass
+
+# Function to return a reversed list of first names
+def get_reversed_first_names(data: str) -> list:
+    pass
